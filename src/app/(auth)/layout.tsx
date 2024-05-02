cd0ef6326fcaebbf52facb7cf6e9cdb59b426974
jbryan83 <james.bryan.home@gmail.com>
2024-05-01 20:44:48 -0500

    Fix useContext error by adding master root layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,4 @@
 import { type Metadata } from "next";
-import { Inter } from "next/font/google";
-
-import { TailwindIndicator } from "@/components/tailwind-indicator";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
-
-import "@/styles/globals.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   title: "Regen Admin - Login",
@@ -23,20 +11,5 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`font-sans ${inter.variable} h-full bg-muted`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <TailwindIndicator />
-          <Toaster visibleToasts={3} />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+  return <div className="bg-muted">{children}</div>;
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import { Inter } from "next/font/google";
+
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+import "@/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`font-sans ${inter.variable} h-full`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <TailwindIndicator />
+          <Toaster visibleToasts={3} />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
