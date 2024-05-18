2c7bfbcafe9cac2c8f700d23c265cd3d09adc71b
jbryan83 <james.bryan.home@gmail.com>
2024-05-17 23:54:07 -0500

    Add strict mode and dev tools script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import { Inter } from "next/font/google";
+// import Script from "next/script";
+import { StrictMode } from "react";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+
 import "@/styles/globals.css";
 
 const inter = Inter({
@@ -15,18 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`font-sans ${inter.variable} h-full`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Toaster visibleToasts={3} />
-        </ThemeProvider>
-      </body>
-    </html>
+    <StrictMode>
+      <html lang="en" suppressHydrationWarning>
+        {/* <Script src="http://192.168.1.34:8097" /> */}
+        <body className={`font-sans ${inter.variable} h-full`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster visibleToasts={3} />
+          </ThemeProvider>
+        </body>
+      </html>
+    </StrictMode>
   );
 }
