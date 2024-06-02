feee108fbfaad75352a11187a7a8510cef028ac1
jbryan83 <james.bryan.home@gmail.com>
2024-06-02 13:08:50 -0500

    Remove date from dashboard layout

--- src/app/(app)/(dashboard)/layout.tsx
+++ src/app/(app)/(dashboard)/layout.tsx
@@ -18,15 +18,6 @@ const DashboardLayout = async ({ children, leader, admin }: DashboardProps) => {
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
       <div className="flex items-center justify-between">
         <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
-        <div
-          className={cn(
-            buttonVariants({ variant: "secondary" }),
-            "flex cursor-default select-none gap-2",
-          )}
-        >
-          <CalendarDays className="h-4 w-4" />
-          {new Date().toLocaleDateString("en-US", { dateStyle: "long" })}
-        </div>
       </div>
       {children}
       {role === "leader" && leader}
